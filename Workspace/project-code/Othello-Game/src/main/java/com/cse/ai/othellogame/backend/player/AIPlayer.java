2b477925fe288ccd032f5e857547b0eed3729969
tasneem-elhady <127930144+tasneem-elhady@users.noreply.github.com>
2024-05-05 12:21:09 +0300

    Update AIPlayer.java
    implement algorithmEval and makeMove

--- Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/backend/player/AIPlayer.java
+++ Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/backend/player/AIPlayer.java
@@ -1,6 +1,7 @@
 package com.cse.ai.othellogame.backend.player;
 
 import com.cse.ai.othellogame.backend.game.Board;
+import com.cse.ai.othellogame.backend.game.DISK;
 
 import java.awt.*;
 
@@ -14,19 +15,15 @@ public class AIPlayer extends Player{
      * The difficulty level of the AI player.
      */
     private final int difficulty;
-    /**
-     * tracker for number of nodes tested by the agent.
-     */
-    public static int nodesExplored;
 
     /**
      * Initializes a new instance of the AIPlayer class with the specified color, game board, and difficulty level.
      *
      * @param board      A reference to the game's board.
-     * @param color      The color assigned to the player. 'B' for black players, 'W' for white players.
+     * @param color      The color assigned to the player. DISK.BLACK for black players, DISK.WHITE for white players.
      * @param difficulty The difficulty level of the AI player. Ranges from 0 to 2, with 2 being the hardest.
      */
-    public AIPlayer(Board board, char color, int difficulty) {
+    public AIPlayer(Board board, DISK color, int difficulty) {
         super(board, color);
         this.difficulty = difficulty;
     }
@@ -35,33 +32,15 @@ public class AIPlayer extends Player{
      * Generates a move for the AI player based on the current state of the board.
      * <p>
      * Warning:
-     * The chosen position must be a position of a hint position ('H') in the board.
+     * The chosen position must be a position of a hint position (DISK.HINT) in the board.
      * </p>
      *
      * @return An integer representing the position chosen by the AI player. (Range: [0, 63])
      * @Note Position = row * 8 + column
      */
     @Override
-    public int makeMove(){
-        return 0;
-    }
-
-        public int makeMove(int [][]board, int player,int depth) {
-        nodesExplored = 0;
-        int bestScore = Integer.MIN_VALUE;
-        Point bestMove = null;
-        for(Point move : minimax.getAllPossibleMoves(board,player)){
-            //create new node
-            int[][] newNode = minimax.getNewBoardAfterMove(board,move,player);
-            //recursive call
-            int childScore = minimax.MMAB(newNode,player,depth-1,false,Integer.MIN_VALUE,Integer.MIN_VALUE);
-            if(childScore > bestScore) {
-                bestScore = childScore;
-                bestMove = move;
-            }
-        }
-        System.out.println("Nodes Explored : " + nodesExplored);
-        return bestMove.x*8+ bestMove.y;
+    public int makeMove() {
+        return algorithmEval();
     }
 
     /**
@@ -73,26 +52,61 @@ public class AIPlayer extends Player{
      *
      * @return None, for now.
      */
-    public int calculateHeuristic(int [][]board,int player){
-        return minimax.evalDiscDiff(board,player);
+    public void calculateHeuristic(){
+        // TODO: implement this method
+
+        // TODO: You can add more data fields to the class to save your values, or you can
+        //       return the heuristic value of each position. Organize your work with the developer of the {@link #algorithmEval}
+        //       method.
+
+        // TODO: Update the documentation of the method when you determine the return type.
     }
 
     /**
      * Executes the AI algorithm to determine the best move.
      * <p>
+     *     <b>Steps</b>
+     *     <ol>
+     *         <li>initialize best score and best move</li>
+     *         <li>iterates on every possible move from this board</li>
+     *         <li>recursively call Algorithm on each board updating child score and comparing to best score existing</li>
+     *         <li>if child score is higher than best score update best score and best move</li>
+     *     </ol>
      * Note:
      * This method is used internally by the AIPlayer class to implement the AI logic.
      * </p>
      *
      * @return None, for now.
      */
-    public void algorithmEval(){
-        // TODO: implement this method
+    public int algorithmEval(){
+        System.out.println(getBoard());
+        int bestScore = Integer.MIN_VALUE;
+        Point bestMove = null;
+        System.out.println("number of possible moves is : " + getBoard().getAllPossibleMoves());
+        for(Point move : getBoard().getAllPossibleMoves()){
 
-        // TODO: You can collaborate with the developer of the {@link #calculateHeuristic} method to utilize
-        //       heuristic values in your algorithm. Additionally, consider adding more data fields to the class or a
-        //       new class to assist in the decision-making process.
-
-        // TODO: Update the documentation of the method when you determine the return type.
+            //create new node
+            Board newNode;
+            try {
+                newNode = (Board) getBoard().clone();
+            } catch (CloneNotSupportedException e) {
+                throw new RuntimeException(e);
+            }
+            newNode.updateBoard(getColor(),move.x,move.y);
+            //recursive call
+//            int childScore = Minimax.MM(newNode,getColor(),2-1,false);
+            int childScore = Minimax.MMAB(newNode,getColor(),2-1,false,Integer.MIN_VALUE,Integer.MIN_VALUE);
+            if(childScore > bestScore) {
+                bestScore = childScore;
+                bestMove = move;
+            }
+        }
+        System.out.println("best move is : " + (bestMove.x*8+ bestMove.y));
+        return bestMove.x*8+ bestMove.y;
+    }
+    public static void main(String[] args){
+        Board board = new Board();
+        AIPlayer ai = new AIPlayer(board,DISK.BLACK,6);
+        ai.makeMove();
     }
 }
