0c29b46888786acdd4bef1b6f9baa7212df56ee8
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2024-05-07 20:33:29 +0300

    add the all unit test for the ai player

---
+++ Workspace/project-code/Othello-Game/src/test/java/com/cse/ai/othellogame/backend/player/HumanPlayerTest.java
@@ -0,0 +1,16 @@
+package com.cse.ai.othellogame.backend.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HumanPlayerTest {
+
+    @Test
+    void setGameGUI() {
+    }
+
+    @Test
+    void makeMove() {
+    }
+}
---
+++ Workspace/project-code/Othello-Game/src/test/java/com/cse/ai/othellogame/backend/player/PlayerTest.java
@@ -0,0 +1,12 @@
+package com.cse.ai.othellogame.backend.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void makeMove() {
+    }
+}
