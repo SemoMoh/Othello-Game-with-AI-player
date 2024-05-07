3b804caade01adc12a8687f14ae4b3bb30247cc6
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2024-05-07 20:12:18 +0300

    start the testing

---
+++ Workspace/project-code/Othello-Game/src/test/java/com/cse/ai/othellogame/backend/player/AIPlayerTest.java
@@ -0,0 +1,20 @@
+package com.cse.ai.othellogame.backend.player;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AIPlayerTest {
+
+    @Test
+    void makeMove() {
+    }
+
+    @Test
+    void calculateHeuristic() {
+    }
+
+    @Test
+    void algorithmEval() {
+    }
+}
