a3efb2ac8d53106986d06fca5b3835b59b14084f
SemoMoh <128331219+SemoMoh@users.noreply.github.com>
2024-05-14 20:03:39 +0300

    Added some docs

--- Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/MainGUI.java
+++ Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/MainGUI.java
@@ -5,42 +5,55 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+
 
 public class MainGUI extends Application {
     public static Stage stage;
     public static Scene scene;
 
+    /**
+     * This method is used to change the scene to the main menu.
+     */
     public static void mainMenu() {
         scene.setRoot(new MainMenu());
+    }
+
+    /**
+     * This method is used to close the game application.
+     */
+    public static void endGame() {
+        stage.close();
+    }
+
+    public static void main(String[] args) {
+        // to stop the windows from scaling the game
+        System.setProperty("prism.allowhidpi", "false");
+        // start the game
+        launch();
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         MainGUI.stage = stage;
         MainGUI.scene = new Scene(new MainMenu(), 1920, 1080);
+
+        // set to full screen
+        stage.setFullScreen(true);
+        // hide full screen hint and stop the game from exiting full screen
         scene.setOnKeyPressed(event -> {
             if (event.getCode() == KeyCode.ESCAPE) {
                 event.consume(); // Consume the event to prevent further processing
             }
         });
-        stage.setScene(scene);
-        stage.setFullScreen(true);
         stage.setFullScreenExitHint("");
         stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+
+        // set the scene and show the stage
+        stage.setScene(scene);
         stage.setTitle("Hello!");
         stage.show();
-    }
-
-    public static void endGame() {
-        stage.close();
-    }
-
-    public static void main(String[] args) {
-        System.setProperty("prism.allowhidpi", "false");
-        launch();
     }
 }
--- Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/gui/resultscreen/ResultScreen.java
+++ Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/gui/resultscreen/ResultScreen.java
@@ -1,7 +1,10 @@
 package com.cse.ai.othellogame.gui.resultscreen;
 
+import com.cse.ai.othellogame.MainGUI;
 import com.cse.ai.othellogame.backend.game.Board;
+import com.cse.ai.othellogame.backend.player.Player;
 import com.cse.ai.othellogame.gui.gamescreen.BoardGUI;
+import com.cse.ai.othellogame.gui.mainmenu.MainMenu;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -14,7 +17,8 @@ import java.util.ResourceBundle;
 public class ResultScreen extends Pane implements Initializable {
     private final Board board;
     private final BoardGUI boardGUI;
-    public ResultScreen(Board board, String playerBlackName, String playerWhiteName, BoardGUI boardGUI) {
+
+    public ResultScreen(Board board, String playerBlackName, String playerWhiteName, BoardGUI boardGUI, Player playerBlack, Player playerWhite) {
         this.board = board;
         this.boardGUI = boardGUI;
 
@@ -38,5 +42,29 @@ public class ResultScreen extends Pane implements Initializable {
     @Override
     public Node getStyleableNode() {
         return super.getStyleableNode();
+    }
+
+    /**
+     * This method is used to close the game application.
+     * When the user clicks the close button, the game will close.
+     */
+    public void closeApp(){
+        MainGUI.endGame();
+    }
+
+    /**
+     * This method is used to go back to the main menu.
+     * When the user clicks the main menu button, the game will go back to the main menu.
+     */
+    public void mainMenu(){
+        MainGUI.mainMenu();
+    }
+
+    /**
+     * This method is used to restart the game.
+     * When the user clicks the restart button, the game will restart.
+     */
+    public void restartGame(){
+        MainMenu.restartTheGame();
     }
 }
