6bd05841e8e559dfde542e39c23ab718988cb2f2
Sarrah-Sherif <131382032+Sarrah-Sherif@users.noreply.github.com>
2024-05-15 22:25:23 +0300

    Update Minimax.java

--- Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/backend/player/Minimax.java
+++ Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/backend/player/Minimax.java
@@ -29,21 +29,14 @@ public class Minimax {
     //returns minimax value for a given node (without A/B pruning)
     public static double MM(Board node, DISK player, int depth, boolean max){
         nodesExplored++;
-        //System.out.println("Nodes Explored : " + nodesExplored);
-
-        //System.out.println("at depth "+ depth+" the board is: ////////////////////////////");
-        //System.out.println(node);
         //if terminal reached or depth limit reached evaluate
         if(depth == 0 || node.gameEnded()){
-//            heuristic calculation
-            //System.out.println("value of heuristic is : " + evalDiscDiff(node,player)+"***************************************************************");
-//            return evalDiscDiff(node,player);
+        //heuristic calculation
             return Heuristic.calculateHeuristic(node.getBoard2D(),player);
         }
         DISK oplayer = (player == DISK.BLACK) ? DISK.WHITE : DISK.BLACK;
         //if no moves available then forfeit turn
         if(node.getAllPossibleMoves().isEmpty()){
-            //System.out.println("Forfeit State Reached !");
             if(max)
                 node.forfeitTurn(player);
             else
@@ -54,7 +47,6 @@ public class Minimax {
         if(max){
             //maximizing
             score = Integer.MIN_VALUE;
-//            //System.out.println("number of possible moves is : " + newNode.getAllPossibleMoves());
             for(Point move : node.getAllPossibleMoves())
             { //my turn
                 Board newNode = new Board();
@@ -65,10 +57,7 @@ public class Minimax {
                     e.printStackTrace();
                 }
                 //create new node
-//                //System.out.println("node before update : **************************************************");
-                newNode.updateBoard(player,move.x,move.y);
-//                //System.out.println("node after update : **************************************************");
-
+               newNode.updateBoard(player,move.x,move.y);
                 //recursive call
                 double childScore = MM(newNode,player,depth-1,false);
                 if(childScore > score) score = childScore;
@@ -78,10 +67,8 @@ public class Minimax {
             //minimizing
 
             score = Integer.MAX_VALUE;
-//            //System.out.println("number of possible moves is : " + newNode.getAllPossibleMoves());
             for(Point move : node.getAllPossibleMoves()){ //opponent turn
                 //create new node
-//                //System.out.println("node before update : **************************************************");
                 Board newNode = new Board();
                 try{
                     newNode = (Board) node.clone();
@@ -89,14 +76,12 @@ public class Minimax {
                     e.printStackTrace();
                 }
                 newNode.updateBoard(oplayer,move.x,move.y);
-//                //System.out.println("node after update : **************************************************");
                 //recursive call
                 double childScore = MM(newNode,player,depth-1,true);
                 if(childScore < score) score = childScore;
             }
         }
-//        //System.out.println("the score is: "+score+"at node "+AIPlayer.nodesExplored);
-        return score;
+     return score;
     }
 
 
@@ -123,20 +108,14 @@ public class Minimax {
      */
 
     public static double MMAB(Board node,DISK player,int depth,boolean max,double alpha,double beta){
-        //System.out.println("Nodes Explored : " + nodesExplored);
         nodesExplored++;
-        //System.out.println("at depth "+ depth+" the board is: ////////////////////////////");
-        //System.out.println(node);
         //if terminal reached or depth limit reached evaluate
         if(depth == 0 || node.gameEnded()){
-            //BoardPrinter bpe = new BoardPrinter(node,"Depth : " + depth);
-//            return evalDiscDiff(node,player);
             return Heuristic.calculateHeuristic(node.getBoard2D(),player);
         }
         DISK oplayer = (player == DISK.BLACK) ? DISK.WHITE : DISK.BLACK;
         //if no moves available then forfeit turn
         if(node.getAllPossibleMoves().isEmpty()){
-            //System.out.println("Forfeit State Reached !");
             if(max)
                 node.forfeitTurn(player);
             else
@@ -163,7 +142,6 @@ public class Minimax {
                 //update alpha
                 if(score > alpha) alpha = score;
                 if(beta <= alpha) {
-                    //System.out.println("---------------------------------------cut off----------------------------------------------");
                     break;
                 } //Beta Cutoff
             }
@@ -186,26 +164,10 @@ public class Minimax {
                 //update beta
                 if(score < beta) beta = score;
                 if(beta <= alpha) {
-                    //System.out.println("alpha: "+ alpha+"beta: "+ beta);
-                    //System.out.println("---------------------------------------cut off----------------------------------------------");
-                    break;
+                     break;
                 } //Alpha Cutoff
             }
         }
-        //System.out.println("the score is: "+score+"\tat node "+nodesExplored);
         return score;
     }
-
-    public static int evalDiscDiff(Board board , DISK player){
-        DISK oplayer = (player==DISK.BLACK) ? DISK.WHITE : DISK.BLACK;
-
-        int mySC = board.getScore(player);
-        int opSC = board.getScore(oplayer);
-//        //System.out.println("the value of node heuristic is "+ 100 * (mySC - opSC) / (mySC + opSC)+"**************************");
-
-        return 100 * (mySC - opSC) / (mySC + opSC);
-    }
-
-
-
 }
