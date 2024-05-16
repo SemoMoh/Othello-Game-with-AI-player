8ce8488a4fbeac2e6500bce43576eed30656a22e
Mahmoud Abdelraouf <mahmoud126125@gmail.com>
2024-05-16 15:01:33 +0300

    add docs

--- Workspace/project-code/Othello-Game/src/main/java/com/cse/ai/othellogame/App.java
+++
@@ -1,7 +0,0 @@
-package com.cse.ai.othellogame;
-
-public class App {
-    public static void main(String[] args) {
-        MainGUI.main(args);
-    }
-}
---
+++ docs/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.cse.ai.othellogame"}];updateSearchResults();
